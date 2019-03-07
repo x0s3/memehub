20c35476dd1ff869d0a1e98ba57fa52be9e3ff41
x0s3 <x0s3.prog@gmail.com>
2019-03-07 20:08:11 +0100

    works with hooks

--- packages/components/src/App.js
+++ packages/components/src/App.js
@@ -1,32 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: 'Actually on iOS',
+  android: 'Actually on Android',
+  web: 'Actually on Web'
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to react-native-web + monorepo!</Text>
-        <Text style={styles.instructions}>This component is being shared between iOS, Android & Web.</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to MemeHub Multiplatform!</Text>
+      <Text style={styles.instructions}>This component is being shared between iOS, Android & Web.</Text>
+      <Text style={styles.instructions}>{instructions}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
