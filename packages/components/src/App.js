1b2dce586f082ec0430a1ef10694c32487f613e8
x0s3 <x0s3.prog@gmail.com>
2019-03-08 09:48:33 +0100

    added rn-paper&vector-icons, wp custom config done

--- packages/components/src/App.js
+++ packages/components/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider, Button, Title } from 'react-native-paper';
 
 const instructions = Platform.select({
   ios: 'Actually on iOS',
@@ -7,14 +8,44 @@ const instructions = Platform.select({
   web: 'Actually on Web'
 });
 
-export default function App() {
+function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <View style={{ alignItems: 'center' }}>
+      <Title>{counter}</Title>
+      <Button onPress={() => setCounter(counter + 1)}>Increment</Button>
+    </View>
+  )
+}
+
+function Welcome() {
   return (
     <View style={styles.container}>
       <Text style={styles.welcome}>Welcome to MemeHub Multiplatform!</Text>
       <Text style={styles.instructions}>This component is being shared between iOS, Android & Web.</Text>
       <Text style={styles.instructions}>{instructions}</Text>
+      <Counter />
     </View>
   );
+}
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <React.Fragment>
+        {Platform.OS === 'web' ? (
+          <style type="text/css">{`
+        @font-face {
+          font-family: 'MaterialIcons';
+          src: url(${require('react-native-vector-icons/Fonts/MaterialIcons.ttf')}) format('truetype');
+        }
+      `}</style>
+        ) : null}
+        <Welcome />
+      </React.Fragment>
+    </PaperProvider>
+  )
 }
 
 const styles = StyleSheet.create({
