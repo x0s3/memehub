d5daf9f83bc9e97de4f77e52134cdc2cb963f730
x0s3 <x0s3.prog@gmail.com>
2019-03-10 14:08:32 +0100

    updated mobile entry

--- packages/mobile/index.js
+++ packages/mobile/index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from 'components/src/App';
+import App from 'components/src/components/App';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
